98e51a43f5f8a229f7ed3c8a648fd8afc30d7935
Sohaib Mohammad <sohaib.mohammad745@gmail.com>
2023-03-18 19:43:31 +0100

    Move routes methods to file

--- api/routes.js
+++ api/routes.js
@@ -926,23 +926,24 @@ function statusesHashtag(app, options, done) {
     done()
 }
 
-export {
-    medias,
-    mediaTypes,
-    statuses,
-    statusesMedias,
-    messages,
-    reactions,
-    messageReactions,
-    users,
-    certifications,
-    relations,
-    interactions,
-    interactionTypes,
-    lists,
-    listUsers,
-    mentions,
-    statusesMentions,
-    hashtags,
-    statusesHashtag,
+export default function registerRoutes(app) {
+    app.get('/', async (request, reply) => { return {} })
+    app.register(medias, { prefix: '/api' })
+    app.register(mediaTypes, { prefix: '/api' })
+    app.register(statuses, { prefix: '/api' })
+    app.register(statusesMedias, { prefix: '/api' })
+    app.register(messages, { prefix: '/api' })
+    app.register(reactions, { prefix: '/api' })
+    app.register(messageReactions, { prefix: '/api' })
+    app.register(users, { prefix: '/api' })
+    app.register(certifications, { prefix: '/api' })
+    app.register(relations, { prefix: '/api' })
+    app.register(interactions, { prefix: '/api' })
+    app.register(interactionTypes, { prefix: '/api' })
+    app.register(lists, { prefix: '/api' })
+    app.register(listUsers, { prefix: '/api' })
+    app.register(mentions, { prefix: '/api' })
+    app.register(statusesMentions, { prefix: '/api' })
+    app.register(hashtags, { prefix: '/api' })
+    app.register(statusesHashtag, { prefix: '/api' })
 }
--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 import Fastify from 'fastify'
-import {
-    medias,
-    mediaTypes,
-    statuses,
-    statusesMedias,
-    messages,
-    reactions,
-    messageReactions,
-    users,
-    certifications,
-    relations,
-    interactions,
-    interactionTypes,
-    lists,
-    listUsers,
-    mentions,
-    statusesMentions,
-    hashtags,
-    statusesHashtag,
-} from './api/routes.js'
+import registerRoutes from './api/routes.js'
 
 const app = Fastify({ logger: true })
 
@@ -31,27 +12,5 @@ const start = async () => {
     }
 }
 
-const registerRoutes = () => {
-    app.get('/', async (request, reply) => { return {} })
-    app.register(medias, { prefix: '/api' })
-    app.register(mediaTypes, { prefix: '/api' })
-    app.register(statuses, { prefix: '/api' })
-    app.register(statusesMedias, { prefix: '/api' })
-    app.register(messages, { prefix: '/api' })
-    app.register(reactions, { prefix: '/api' })
-    app.register(messageReactions, { prefix: '/api' })
-    app.register(users, { prefix: '/api' })
-    app.register(certifications, { prefix: '/api' })
-    app.register(relations, { prefix: '/api' })
-    app.register(interactions, { prefix: '/api' })
-    app.register(interactionTypes, { prefix: '/api' })
-    app.register(lists, { prefix: '/api' })
-    app.register(listUsers, { prefix: '/api' })
-    app.register(mentions, { prefix: '/api' })
-    app.register(statusesMentions, { prefix: '/api' })
-    app.register(hashtags, { prefix: '/api' })
-    app.register(statusesHashtag, { prefix: '/api' })
-}
-
-registerRoutes()
+registerRoutes(app)
 start()
